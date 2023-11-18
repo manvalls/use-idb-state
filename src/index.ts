df92a46caeafe2c364d07ee4921f1eb7628e36e0
Manuel Valls <me@manvalls.com>
2023-11-19 00:11:24 +0100

    Add storage event

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { UseStore, get, set, del } from "idb-keyval";
+
+const STORAGE_KEY = "use-idb-state-ts";
 
 export default function useIDBState<T>(
   key: IDBValidKey,
@@ -7,8 +9,17 @@ export default function useIDBState<T>(
   customStore?: UseStore
 ) {
   const [state, setState] = useState<T>(initialValue);
+  const storedState = useRef<T | undefined>(undefined);
   const [loading, setLoading] = useState(true);
   const [currentKey, setCurrentKey] = useState(key);
+  const [ts, setTs] = useState(Date.now());
+
+  const emitStorageEvent = (value: T | undefined) => {
+    storedState.current = value;
+    try {
+      localStorage.setItem(STORAGE_KEY, Date.now().toString(36));
+    } catch (e) {}
+  };
 
   useEffect(() => {
     let mounted = true;
@@ -18,38 +29,76 @@ export default function useIDBState<T>(
     }
 
     setLoading(true);
-    get<T>(key, customStore).then((value) => {
-      if (!mounted) {
-        return;
-      }
+    get<T>(key, customStore)
+      .then((value) => {
+        if (!mounted) {
+          return;
+        }
 
-      if (value !== undefined) {
-        setState(value);
-      } else if (state !== undefined) {
-        set(key, state, customStore);
-      }
+        if (value !== undefined) {
+          storedState.current = value;
+          setState(value);
+          return;
+        }
 
-      setLoading(false);
-      setCurrentKey(key);
-    });
+        if (state !== undefined) {
+          return set(key, state, customStore).then(() => {
+            emitStorageEvent(state);
+          });
+        }
+
+        storedState.current = undefined;
+      })
+      .finally(() => {
+        if (!mounted) {
+          return;
+        }
+
+        setCurrentKey(key);
+        setLoading(false);
+      });
 
     return () => {
       mounted = false;
     };
-  }, [key]);
+  }, [key, ts]);
 
   useEffect(() => {
     if (loading) {
       return;
     }
 
-    if (state === undefined) {
-      del(currentKey, customStore);
+    if (state === storedState.current) {
       return;
     }
 
-    set(currentKey, state, customStore);
+    (() => {
+      if (state === undefined) {
+        return del(currentKey, customStore);
+      }
+
+      return set(currentKey, state, customStore);
+    })().then(() => {
+      emitStorageEvent(state);
+    });
   }, [currentKey, state, loading]);
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const handleStorage = (e: StorageEvent) => {
+      if (e.key === STORAGE_KEY) {
+        setTs(Date.now());
+      }
+    };
+
+    window.addEventListener("storage", handleStorage);
+    return () => {
+      window.removeEventListener("storage", handleStorage);
+    };
+  }, []);
 
   return [state, setState, loading] as const;
 }
